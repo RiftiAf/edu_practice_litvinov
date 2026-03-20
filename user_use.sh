5f60964574c30802cf40c5be168c029412d25cc1
Rifti <bqtoper5021@gmail.com>
2026-03-21 00:25:30 +0700

    Delete user_use.sh

--- user_use.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Топ пользователей по использованию дискового пространства в /home:"
-if [ -d "/home" ]; then
-    du -sk /home/* 2>/dev/null | sort -rn | while read size path; do
-        user=$(basename "$path")
-        size_mb=$((size / 1024))
-        echo "$user: ${size_mb} MB"
-    done
-fi
