1501292afe42133c094a252f7ff5d864f65f06a1
Rifti <bqtoper5021@gmail.com>
2026-03-21 00:25:37 +0700

    Delete zero.sh

--- zero.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-group="9СА-324К"
-name="Иван"
-fama="Литвинов"
-money=0
-money1=84
-
-echo "Я учусь в $group , зовут меня $name $fama . В этом месяце мне пришла стипендия размером в $money рублей, это $((money / money1))$."
