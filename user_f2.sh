5edb3236697785036bdf976e68f7da9fb4f239c6
Rifti <bqtoper5021@gmail.com>
2026-03-21 00:25:12 +0700

    Delete user_f2.sh

--- user_f2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if grep -q "^$1:" /etc/passwd; then
-    echo "Пользователь $1 найден!"
-else
-    touch ~/dont_be_sad_user_${1}_will_be_there_soon.txt
-    echo "Создан файл для $1"
-fi
