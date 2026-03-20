e500cac77c54c2ac23bea03aa7169c8d20ab95cd
Rifti <bqtoper5021@gmail.com>
2026-03-21 00:23:47 +0700

    Delete dir_info.sh

--- dir_info.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dir="${1:-.}"
-if [ ! -d "$dir" ]; then
-    echo "Ошибка: директория '$dir' не существует!"
-    exit 1
-fi
-size=$(du -sk "$dir" 2>/dev/null | awk '{print $1}')
-echo "Общий размер директории $dir: ${size} KB"
